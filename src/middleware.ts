ce619f7e8a917e1b6beb4e8e8ff49d1a3b90352f
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-23 15:54:55 +0700

    + middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,14 @@ import { User } from "../next-auth";
 
 async function Middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const originalUrl = request.nextUrl.protocol + request.headers.get('host') + request.nextUrl.pathname
+  const originalUrl = request.nextUrl.protocol + process.env.NEXTAUTH_URL + request.nextUrl.pathname
   const token: User = await getToken({ req: request }) as any;
 
   if (pathname == paths.auth.signIn && !token) return NextResponse.next();
-  if (pathname == paths.auth.signIn && token) return NextResponse.redirect(new URL(paths.admin.overview, originalUrl))
-  if (pathname == "/") return NextResponse.redirect(new URL(paths.admin.overview, originalUrl));
-  if (pathname.startsWith(paths.admin.overview) && !token) return NextResponse.redirect(new URL(paths.auth.signIn, originalUrl));
-  if ((pathname.startsWith(paths.admin.admin) || pathname.startsWith(paths.admin.payment)) && token.status == 0) return NextResponse.redirect(new URL(paths.admin.overview, originalUrl));
+  if (pathname == paths.auth.signIn && token) return NextResponse.rewrite(new URL(paths.admin.overview, originalUrl))
+  if (pathname == "/") return NextResponse.rewrite(new URL(paths.admin.overview, originalUrl));
+  if (pathname.startsWith(paths.admin.overview) && !token) return NextResponse.rewrite(new URL(paths.auth.signIn, originalUrl));
+  if ((pathname.startsWith(paths.admin.admin) || pathname.startsWith(paths.admin.payment)) && token.status == 0) return NextResponse.rewrite(new URL(paths.admin.overview, originalUrl));
 
   return NextResponse.next();
 }
