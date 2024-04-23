8baf53139c21ae71addf3047c4dd50bc1fea9d4d
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-23 15:33:02 +0700

    + fix url

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -28,10 +28,7 @@ export const ParseActivity = async (payload: ActivityPayload) => {
 }
 
 export function getSiteURL(): string {
-  let url =
-    process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/';
+  let url = process.env.NEXTAUTH_URL ?? window.location.hostname;
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,32 +5,14 @@ import { User } from "../next-auth";
 
 async function Middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const token = await getToken({ req: request });
-  const protector = {
-    user: [paths.home as string],
-    admin: paths.admin.overview,
-  }
+  const originalUrl = request.nextUrl.protocol + request.headers.get('host') + request.nextUrl.pathname
+  const token: User = await getToken({ req: request }) as any;
 
-  if (pathname == "/") return NextResponse.redirect(new URL(paths.admin.overview, request.url))
   if (pathname == paths.auth.signIn && !token) return NextResponse.next();
-  if (pathname == paths.auth.signIn && token) return NextResponse.redirect(new URL(paths.home, request.url))
-
-  if (
-    token == null &&
-    (
-      protector.user.includes(pathname) ||
-      pathname.startsWith(protector.admin)
-    )
-  ) {
-    return NextResponse.redirect(new URL(paths.auth.signIn, request.url))
-  }
-
-  const data: User = token as any;
-
-  // admin access user
-  if (protector.user.includes(pathname) && data.status == 0) return NextResponse.redirect(new URL(paths.admin.overview, request.url));
-  // admin access ceo 
-  if ((pathname.startsWith(paths.admin.admin) || pathname.startsWith(paths.admin.payment)) && data.status == 0) return NextResponse.redirect(new URL(paths.admin.overview, request.url));
+  if (pathname == paths.auth.signIn && token) return NextResponse.redirect(new URL(paths.admin.overview, originalUrl))
+  if (pathname == "/") return NextResponse.redirect(new URL(paths.admin.overview, originalUrl));
+  if (pathname.startsWith(paths.admin.overview) && !token) return NextResponse.redirect(new URL(paths.auth.signIn, originalUrl));
+  if ((pathname.startsWith(paths.admin.admin) || pathname.startsWith(paths.admin.payment)) && token.status == 0) return NextResponse.redirect(new URL(paths.admin.overview, originalUrl));
 
   return NextResponse.next();
 }
