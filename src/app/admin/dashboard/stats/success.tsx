8f4cccf3892262300728bf1bf272e1b9dd187cb2
iwind <134185860+iwindd@users.noreply.github.com>
2024-05-03 11:34:03 +0700

    stats click

--- src/app/admin/dashboard/stats/success.tsx
+++ src/app/admin/dashboard/stats/success.tsx
@@ -1,7 +1,9 @@
+import { paths } from '@/paths';
 import { CheckTwoTone } from '@mui/icons-material';
-import { Avatar, Card, CardContent, Stack, Typography } from '@mui/material';
+import { Avatar, Card, CardContent, Link, Stack, Typography } from '@mui/material';
 import type { SxProps } from '@mui/material/styles';
 import * as React from 'react';
+import RouterLink from 'next/link';
 
 export interface TotalSuccessInvoiceProps {
   sx?: SxProps;
@@ -10,20 +12,26 @@ export interface TotalSuccessInvoiceProps {
 
 export function TotalSuccessInvoice({ value, sx }: TotalSuccessInvoiceProps): React.JSX.Element {
   return (
-    <Card sx={sx}>
-      <CardContent>
-        <Stack direction="row" sx={{ alignItems: 'flex-start', justifyContent: 'space-between' }} spacing={3}>
-          <Stack spacing={1}>
-            <Typography color="text.secondary" variant="overline">
-              บิลที่สำเร็จ
-            </Typography>
-            <Typography variant="h4">{value}</Typography>
+    <Link
+      underline="none"
+      component={RouterLink}
+      href={`${paths.admin.invoice}?fStatus=1`}
+    >
+      <Card sx={sx}>
+        <CardContent>
+          <Stack direction="row" sx={{ alignItems: 'flex-start', justifyContent: 'space-between' }} spacing={3}>
+            <Stack spacing={1}>
+              <Typography color="text.secondary" variant="overline">
+                บิลที่สำเร็จ
+              </Typography>
+              <Typography variant="h4">{value}</Typography>
+            </Stack>
+            <Avatar sx={{ backgroundColor: 'var(--mui-palette-success-main)', height: '56px', width: '56px' }}>
+              <CheckTwoTone />
+            </Avatar>
           </Stack>
-          <Avatar sx={{ backgroundColor: 'var(--mui-palette-success-main)', height: '56px', width: '56px' }}>
-            <CheckTwoTone />
-          </Avatar>
-        </Stack>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
