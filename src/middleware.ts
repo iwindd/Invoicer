f7f7b880524d05eb3323b001b2df791b2c9cb22c
iwindd <freewinendlife@gmail.com>
2024-06-16 20:46:04 +0700

    chore: regex standalone

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,53 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { pathToRegexp } from './libs/regexp';
-import { getToken } from 'next-auth/jwt';
-import { paths } from './paths';
-import { User } from '../next-auth';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { paths } from "./paths";
+import { User } from "../next-auth";
 
 export async function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname || '/';
-  const token: User = await getToken({ req: request }) as any;
+  const path = request.nextUrl.pathname || "/";
+  const token: User = (await getToken({ req: request })) as any;
 
   function matches(pattern: string): boolean {
-    return pathToRegexp(pattern).test(path);
+    const modifiedPattern = pattern
+      .replace(/\/:\w+\*/g, "(?:/.*)?")
+      .replace(/:\w+/g, "[^/]+")
+      .replace(/\//g, "\\/");
+    const regex = new RegExp(`^${modifiedPattern}$`);
+    const state = regex.test(path);
+
+    return state;
   }
 
   // redirect auth
-  if (matches("/auth/:path*")){
+  if (matches("/auth/:path*")) {
     if (token) {
       return NextResponse.rewrite(new URL(paths.admin.overview, request.url));
     }
-  }else{
+  } else {
     if (!token) {
       return NextResponse.rewrite(new URL(paths.auth.signIn, request.url));
     }
   }
 
-  // redirect index 
-  if (matches("/") || !token){
+  // redirect index
+  if (matches("/") || !token) {
     if (token) {
       return NextResponse.rewrite(new URL(paths.admin.overview, request.url));
-    }else{
+    } else {
       return NextResponse.rewrite(new URL(paths.auth.signIn, request.url));
     }
   }
 
   // admin guard
-  if (matches("/admin/payment/:path*") || matches("/admin/admin/:path*")){
+  if (matches("/admin/payment/:path*") || matches("/admin/admin/:path*")) {
     if (token?.status != 1) {
       return NextResponse.rewrite(new URL(paths.admin.overview, request.url));
     }
   }
 
   // root guard
-  if (matches("/admin/applications/:path*")){
+  if (matches("/admin/applications/:path*")) {
     if (token?.root != true) {
       return NextResponse.rewrite(new URL(paths.admin.overview, request.url));
     }
@@ -59,5 +65,5 @@ export const config = {
    * 4. /_vercel (Vercel internals)
    * 5. Static files (e.g. /favicon.ico, /sitemap.xml, /robots.txt, etc.)
    */
-  matcher: ['/((?!api/|_next/|_static|_vercel|[\\w-]+\\.\\w+).*)'],
+  matcher: ["/((?!api/|_next/|_static|_vercel|[\\w-]+\\.\\w+).*)"],
 };
