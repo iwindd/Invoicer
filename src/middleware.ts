9820d4fc3c5b1ab3054aba9c27d0f073400ccb13
iwindd <freewinendlife@gmail.com>
2024-06-17 00:17:34 +0700

    refactor: use startWith

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,27 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from "next-auth/jwt";
 import { paths } from "./paths";
 import { User } from "../next-auth";
 
 export async function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname || "/";
-  const originalUrl = process.env.NEXTAUTH_URL + request.nextUrl.pathname
+  const path = request.nextUrl.pathname ;
+  const originalUrl = process.env.NEXTAUTH_URL + path
   const token: User = (await getToken({ req: request })) as any;
 
-  function matches(pattern: string): boolean {
-    const modifiedPattern = pattern
-      .replace(/\/:\w+\*/g, "(?:/.*)?")
-      .replace(/:\w+/g, "[^/]+")
-      .replace(/\//g, "\\/");
-    const regex = new RegExp(`^${modifiedPattern}$`);
-    const state = regex.test(path);
-
-    return state;
-  }
-
   // redirect auth
-  if (matches("/auth/:path*")) {
+  if (path.startsWith("/auth")){
     if (token) {
       return NextResponse.rewrite(new URL(paths.admin.overview, originalUrl));
     }
-  } else {
+  }else{
     if (!token) {
       return NextResponse.rewrite(new URL(paths.auth.signIn, originalUrl));
     }
   }
 
+
   // redirect index
-  if (matches("/") || !token) {
+  if (path == "/"){
     if (token) {
       return NextResponse.rewrite(new URL(paths.admin.overview, originalUrl));
     } else {
@@ -41,14 +30,14 @@ export async function middleware(request: NextRequest) {
   }
 
   // admin guard
-  if (matches("/admin/payment/:path*") || matches("/admin/admin/:path*")) {
+  if (path.startsWith("/admin/payment") || path.startsWith("/admin/admin")){
     if (token?.status != 1) {
       return NextResponse.rewrite(new URL(paths.admin.overview, originalUrl));
     }
   }
 
   // root guard
-  if (matches("/admin/applications/:path*")) {
+  if (path.startsWith("/admin/payment")){
     if (token?.root != true) {
       return NextResponse.rewrite(new URL(paths.admin.overview, originalUrl));
     }
